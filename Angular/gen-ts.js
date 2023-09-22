1cd536331d08dc027380b05ab40b9028764d05f0
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2023-09-21 21:49:33 -0400

    Adding Angular Sample Project

---
+++ Angular/gen-ts.js
@@ -0,0 +1,7 @@
+const generator = require("openapi-ts-generator");
+
+generator.generateTsModels({
+  openApiJsonUrl: "http://localhost:5161/swagger/v1/swagger.json",
+  outputPath: "./src/models/",
+  genAngularFormGroups: true /* Set this to true if only if you're in an Angular project*/,
+});
---
+++ Angular/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    ReactiveFormsModule,
+  ],
+  providers: [DatePipe],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
