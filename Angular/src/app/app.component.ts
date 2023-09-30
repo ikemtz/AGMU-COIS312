bf8cd5a7c79085b1ebee1edd9d284c775d454c97
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2023-09-30 16:23:40 -0400

    Adding StudentClass SubGrid to Angular

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,25 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Observable, delay } from 'rxjs';
-import { IStudent, StudentProperties } from 'src/models';
+import { ClassProperties, IStudent, StudentClassProperties, StudentProperties } from 'src/models';
 import { MatDialog } from '@angular/material/dialog';
 import { AddEditStudentComponent } from './add-edit-student/add-edit-student.component';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),]
 })
 export class AppComponent {
   public studentData$: Observable<IStudent[]>;
   public props = StudentProperties;
 
-  public readonly displayedColumns: string[] = [
+  public expandedElement: IStudent | null = null;
+  public readonly columnsToDisplay: string[] = [
+    'expand',
     StudentProperties.ID,
     StudentProperties.ACADEMIC_PROGRAM_ID,
     StudentProperties.NAME,
+    StudentProperties.PHONE_NUMBER,
     'edit'
   ];
+  public readonly subColumnsToDisplay: string[] = [
+    StudentClassProperties.CLASS,
+    StudentClassProperties.GRADE,
+    ClassProperties.STAFF
+  ];
+
   constructor(
     private readonly httpClient: HttpClient,
     private readonly dialog: MatDialog) {
@@ -29,7 +45,7 @@ export class AppComponent {
   private openDialog(student?: IStudent) {
     const dialogRef = this.dialog.open(AddEditStudentComponent, {
       data: student,
-      height: '500px',
+      height: '320px',
       width: '600px',
     });
 
@@ -43,5 +59,12 @@ export class AppComponent {
   }
   public editStudent(student: IStudent) {
     this.openDialog(student);
+  }
+  public expandRow(element: IStudent) {
+    if (this.expandedElement == element) {
+      this.expandedElement = null;
+    } else {
+      this.expandedElement = element;
+    }
   }
 }
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +13,7 @@ import { AddEditStudentComponent } from './add-edit-student/add-edit-student.com
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSelectModule } from '@angular/material/select';
-
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
 
@@ -37,6 +36,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatDatepickerModule,
     MatSelectModule,
     MatNativeDateModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [DatePipe,
 
