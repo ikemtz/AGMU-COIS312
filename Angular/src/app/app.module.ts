ce050bb31a47579275cfaf5729ffc7336950f015
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2023-10-06 10:22:42 -0400

    Removing Angular routing

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
@@ -24,7 +23,6 @@ import { MatNativeDateModule } from '@angular/material/core';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatTableModule,
